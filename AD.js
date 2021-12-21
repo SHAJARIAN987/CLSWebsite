fd37c5d67b1cc064eb40bb82fbd2fa78ca93c1c2
SHAJARIAN987 <88212606+SHAJARIAN987@users.noreply.github.com>
2021-12-20 19:01:19 -0500

    Add files via upload

---
+++ AD.js
@@ -0,0 +1,9 @@
+function i(){
+    var x=document.getElementById("AD");
+    var t=' ';
+    var b;
+    for(b=0;b<9;b+1){
+      t+=x.elements[i].value+"<br>";
+    }
+  document.getElementById("endr").innerHTML=t; 
+   }        
